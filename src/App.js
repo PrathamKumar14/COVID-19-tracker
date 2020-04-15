a0176c72845621f30827f4cfa207a98bfe0996ac
Pratham kumar <prathamsolera@gmail.com>
2020-04-16 01:07:21 +0530

    Components Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Cards from "./components/Cards.jsx";
+import Chart from "./components/Chart.jsx";
+import CountryPicker from "./components/CountryPicker.jsx";
+import styles from "./App.module.css"
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Test</h1>
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     )
   }
